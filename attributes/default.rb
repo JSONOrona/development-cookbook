8bfb596fb651e25b6051e1ab1a96680c902cd858
jorona <jason.orona@viverae.com>
2016-03-17 21:23:54 -0500

    add remote file url, not done

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,19 @@
+# Files URL
+default['development']['files_url'] = 'www.example.com'
+
+# Workspace
+default['development']['workspace'] = '~/workspace/archive'
+
+# MySQL version
+default['development']['mysql_version'] = 'mysql56'
+
+# OpenJDK settings
+default['java']['install_flavor'] = 'openjdk'
+default['java']['jdk_version'] = 8
+
 # Intellij settings
 default['idea']['version'] = '15.0.2'
 default['idea']['edition'] = 'C'
 default['idea']['setup_dir'] = '/opt'
 default['idea']['64bits']['Xmx'] = '2048m'
 default['idea']['64bits']['Xms'] = '2048m'
-# OpenJDK settings
-default['java']['install_flavor'] = 'openjdk'
-default['java']['jdk_version'] = 8
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,9 +4,25 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 #
+mysql_version = node['development']['mysql_version']
+
 include_recipe 'yum::default'
 include_recipe 'build-essential::default'
 include_recipe 'virtualbox-guest-additions'
 include_recipe 'git::default'
-include_recipe 'yum-mysql-community::mysql56'
 include_recipe 'development-cookbook::idea'
+include_recipe "yum-mysql-community::#{mysql_version}"
+
+directory '/opt/workspace' do
+  owner 'vagrant'
+  group 'vagrant'
+  mode '0777'
+  action :create
+end
+
+remote_file '/opt/workspace' do
+  source node['development']['files_url']
+  mode '0777'
+  owner 'vagrant'
+  action :create
+end
